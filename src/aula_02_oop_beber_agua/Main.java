741655e25fb76e6588eac98b02f39c3a37f141dc
Cristian Silvera <cristiansilveraa@gmail.com>
2023-02-18 23:28:51 -0300

    Unit 2 example 02 oop

---
+++ src/aula_02_oop_beber_agua/Main.java
@@ -0,0 +1,17 @@
+package aula_02_oop_beber_agua;
+
+import aula_02_oop_caneta.Caneta;
+
+public class Main {
+    public static void main(String[] args) {
+        BeberAgua b1 = new BeberAgua();
+        b1.marca = "Salus";
+        b1.litros = 1;
+        b1.porcentaje = 90;
+        b1.tapada = false;
+
+        b1.status();
+        b1.beber();
+
+    }
+}
