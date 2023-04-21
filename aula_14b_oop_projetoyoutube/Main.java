b31a04fa0b5ea752ac44f5cf548ab35d08cf4b60
Cristian Silvera <cristiansilveraa@gmail.com>
2023-04-21 10:31:51 -0300

    docs: Practice part 3 OOP and end of project

--- aula_14b_oop_projetoyoutube/Main.java
+++ aula_14b_oop_projetoyoutube/Main.java
@@ -8,13 +8,30 @@ public class Main {
 
         Gafanhoto g[] = new Gafanhoto[2];
         g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
-        g[0] = new Gafanhoto("Creouza", 12, "F", "creo");
+        g[1] = new Gafanhoto("Creouza", 12, "F", "creo");
 
+       /*
+        System.out.println("VIDEOS\n---------------------------------------------");
         System.out.println(v[0].toString());
         System.out.println(v[1].toString());
         System.out.println(v[2].toString());
-
+        System.out.println("\nGAFANHOTOS\n---------------------------------------");
         System.out.println(g[0].toString());
+        System.out.println(g[1].toString());
+        */
+
+        Visualizacao visuali [] = new Visualizacao [5];
+        visuali[0] = new Visualizacao (g[0], v[2]); // juba assiste HTML5
+        visuali[0].avaliar();
+        System.out.println(visuali[0].toString());
+
+
+        visuali[1] = new Visualizacao (g[0], v[1]); // juba assiste PHP
+        visuali[1].avaliar(87.0f);
+        System.out.println(visuali[1].toString());
+
+
+
 
     }
 }
